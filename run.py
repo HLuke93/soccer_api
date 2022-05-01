383f99832974bcab79a3f3cfd6fea252bf816c57
HLuke93 <lukehickson2020@gmail.com>
2022-05-01 18:15:04 +0000

    add football api code and link

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import requests
+
+url = "https://api-football-v1.p.rapidapi.com/v2/countries"
+
+headers = {
+    'x-rapidapi-host': "api-football-v1.p.rapidapi.com",
+    'x-rapidapi-key': "64f751dfa5mshee26054ee346f0ap16a79ejsn3b246371d676"
+    }
+
+response = requests.request("GET", url, headers=headers)
+
+print(response.text)
